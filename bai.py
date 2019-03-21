a701129ec5129b2f6adc9c6713882a7cb476015a
zw1nc3k <zwincek@hotmail.com>
2019-03-21 15:29:35 +0100

    Update bai.py

--- bai.py
+++ bai.py
@@ -5,7 +5,7 @@ lista.append(input("Podaj Nazwisko 3: "))
 lista.append(input("Podaj Nazwisko 4: "))
 lista.append(input("Podaj Nazwisko 5: "))
 lista2 = [item for item in lista if
-          (item[0] == "Q") or (item[0] == "R") or (item[0] == "R") or (item[0] == "R") or (item[0] == "S") or (
+          (item[0] == "Q") or (item[0] == "R") or (item[0] == "S") or (
                   item[0] == "T") or (item[0] == "U") or (item[0] == "V") or (item[0] == "W") or (
                   item[0] == "X") or (item[0] == "Y") or (item[0] == "Z")]
 print(lista2)
