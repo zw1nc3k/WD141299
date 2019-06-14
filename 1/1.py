92f9ec8f5ab1ae9eb772667d511616b0285d2c2c
zw1nc3k <zwincek@hotmail.com>
2019-06-14 20:25:25 +0200

    Update 1.py

--- 1/1.py
+++ 1/1.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-data = pd.read_csv("wc.csv", index_col="Team", sep=';')
+data = pd.read_csv("wc.csv", index_col="Name", sep=';')
+data2 = data.drop(columns="Position")
+data2 = data2.drop(columns="Group")
+data2 = data2.drop(columns="Type")
+data2 = data2.drop(columns="Team")
+data2 = data2.drop(columns="Country and Club")
 golegrupowe = data.groupby(['Group']).agg({'Goals' : ['sum']})
 wykres = golegrupowe.plot.pie(subplots=True)
 plt.show()
+zawodnicy10=data2[data2["Goals"] > 10]
+zawodnicy10.groupby(["Name"])
+print(zawodnicy10)
